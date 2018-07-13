e42df1087f4e705bc116946e206cba7139fc2e6e
Steven Meyer <steven@stdlib.com>
2018-07-13 13:37:38 -0700

    Commited by Code.xyz

--- functions/__main__.js
+++ functions/__main__.js
@@ -1,23 +1,6 @@
-
-
-
-
 /**
 * A simple "hello world" function
 */
-module.exports = async (
-  
-  
-  name = 'world') => {
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = async (name = 'world') => {
+  return `Hello ${name}, I built this API with StdLib and Code xyz!`;
+};
